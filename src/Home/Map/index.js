8c1545a32143f3e5ac0630c4baf4ef0692b7ff93
Darshak Patel <pateldarshak2636@gmail.com>
2022-08-06 23:32:20 +0530

    Polyline Color

--- src/Home/Map/index.js
+++ src/Home/Map/index.js
@@ -20,25 +20,29 @@ class Map extends React.Component {
       <LoadScript googleMapsApiKey="AIzaSyAUq5AvYt0S4uG86QRy6uA0t7i0JOqpx-c">
         <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
           {this.props.selectedRoutesCord &&
-            this.props.selectedRoutesCord.map(path => (
-              <Polyline
-                path={path}
-                options={{
-                  strokeColor: '#FF0000',
-                  strokeOpacity: 0.8,
-                  strokeWeight: 2,
-                  fillColor: '#FF0000.',
-                  fillOpacity: 0.35,
-                  clickable: false,
-                  draggable: false,
-                  editable: false,
-                  visible: true,
-                  radius: 30000,
-                  paths: path,
-                  zIndex: 1,
-                }}
-              />
-            ))}
+            this.props.selectedRoutesCord.map(path => {
+              const color =
+                '#' + Math.floor(Math.random() * 16777215).toString(16);
+              return (
+                <Polyline
+                  path={path}
+                  options={{
+                    strokeColor: color,
+                    strokeOpacity: 0.8,
+                    strokeWeight: 2,
+                    fillColor: color,
+                    fillOpacity: 0.35,
+                    clickable: false,
+                    draggable: false,
+                    editable: false,
+                    visible: true,
+                    radius: 30000,
+                    paths: path,
+                    zIndex: 1,
+                  }}
+                />
+              );
+            })}
         </GoogleMap>
       </LoadScript>
     );
